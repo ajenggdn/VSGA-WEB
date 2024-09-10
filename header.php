8b0bd22d6ab02e18b0262646b710b0e9ee06fb4f
ajenggdn <145746946+ajenggdn@users.noreply.github.com>
2024-09-10 15:43:31 +0700

    Add files via upload

---
+++ header.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="shortcut icon" href="assets/img/favicon.png" type="image/png">
+
+    <!--=============== REMIXICONS ===============-->
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
+    
+    <!--=============== SWIPER CSS ===============-->
+    <link rel="stylesheet" href="assets/css/swiper-bundle.min.css">
+
+    <!--=============== CSS ===============-->
+    <link rel="stylesheet" href="assets/css/styles.css">
+
+    <title>Responsive Website Travel</title>
+</head>
+<body>
+    <header class="header" id="header">
+        <nav class="nav container">
+            <a href="#" class="nav__logo">Travel</a>
+
+            <div class="nav__menu" id="nav-menu">
+                <ul class="nav__list">
+                    <li class="nav__item">
+                        <a href="#home" class="nav__link active-link">Home</a>
+                    </li>
+                   
+                    <li class="nav__item">
+                        <a href="#gallery" class="nav__link">Gallery</a>
+                    </li>
+                    <li class="nav__item">
+                        <a href="#video" class="nav__link">Video</a>
+                    </li>
+                    <li class="nav__item">
+                        <a href="#place" class="nav__link">Tour</a>
+                    </li>
+                </ul>
+
+                <div class="nav__dark">
+                    <!-- Theme change button -->
+                    <span class="change-theme-name">Dark mode</span>
+                    <i class="ri-moon-line change-theme" id="theme-button"></i>
+                </div>
+
+                <i class="ri-close-line nav__close" id="nav-close"></i>
+            </div>
+
+            <div class="nav__toggle" id="nav-toggle">
+                <i class="ri-function-line"></i>
+            </div>
+        </nav>
+    </header>
---
+++ index.php
@@ -0,0 +1,241 @@
+<?php include('header.php'); ?>
+
+<main class="main">
+    <!--==================== HOME ====================-->
+    <section class="home" id="home">
+        <img src="assets/img/img3.jpg" alt="" class="home__img">
+
+        <div class="home__container container grid">
+            <div class="home__data">
+                <span class="home__data-subtitle">Discover your place</span>
+                <h1 class="home__data-title">Explore The <br> Best <b>World <br> Tourism</b></h1>
+                <a href="#" class="button">Explore</a>
+            </div>
+
+            <div class="home__social">
+                <a href="https://www.facebook.com/" target="_blank" class="home__social-link">
+                    <i class="ri-facebook-box-fill"></i>
+                </a>
+                <a href="https://www.instagram.com/" target="_blank" class="home__social-link">
+                    <i class="ri-instagram-fill"></i>
+                </a>
+                <a href="https://twitter.com/" target="_blank" class="home__social-link">
+                    <i class="ri-twitter-fill"></i>
+                </a>
+            </div>
+
+
+        </div>
+    </section>
+
+    <section class="discover section" id="gallery">
+        <h2 class="section__title">Discover the most <br> attractive places</h2>
+
+        <div class="discover__container container swiper-container">
+            <div class="swiper-wrapper">
+                <!--==================== DISCOVER 1 ====================-->
+                <div class="discover__card swiper-slide">
+                    <img src="assets/img/img6.jpg" alt="" class="discover__img">
+                    <div class="discover__data">
+                        <h2 class="discover__title">Barcelona</h2>
+                        <span class="discover__description">24 tours available</span>
+                    </div>
+                </div>
+
+                <div class="discover__card swiper-slide">
+                    <img src="assets/img/img5.jpg" alt="" class="discover__img">
+                    <div class="discover__data">
+                        <h2 class="discover__title">Ibza</h2>
+                        <span class="discover__description">15 tours available</span>
+                    </div>
+                </div>
+
+                <div class="discover__card swiper-slide">
+                    <img src="assets/img/img7.jpg" alt="" class="discover__img">
+                    <div class="discover__data">
+                        <h2 class="discover__title">Madrid</h2>
+                        <span class="discover__description">32 tours available</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+    <section class="video section" id="video">
+        <h2 class="section__title">Video Tour</h2>
+
+        <div class="video__container container">
+            <p class="video__description">Find out more with our video of the most beautiful and
+                pleasant places for you and your family.
+            </p>
+
+            <div class="video__content">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/C911U_Fo-QU?si=lOe4w4DMkCGli1DH" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+            </div>
+        </div>
+    </section>
+
+
+    <!--==================== PLACES ====================-->
+    <section class="place section" id="place">
+        <h2 class="section__title">Choose Your Place</h2>
+
+        <div class="place__container container grid">
+            <!--==================== PLACES CARD 1 ====================-->
+            <div class="place__card">
+                <img src="assets/img/place1.jpg" alt="" class="place__img">
+
+                <div class="place__content">
+                    <span class="place__rating">
+                        <i class="ri-star-line place__rating-icon"></i>
+                        <span class="place__rating-number">4,8</span>
+                    </span>
+
+                    <div class="place__data">
+                        <h3 class="place__title">Bali</h3>
+                        <span class="place__subtitle">Indonesia</span>
+                        <span class="place__price">$2499</span>
+                    </div>
+                </div>
+
+                <button class="button button--flex place__button">
+                    <i class="ri-arrow-right-line"></i>
+                </button>
+            </div>
+
+            <!--==================== PLACES CARD 2 ====================-->
+            <div class="place__card">
+                <img src="assets/img/place2.jpg" alt="" class="place__img">
+
+                <div class="place__content">
+                    <span class="place__rating">
+                        <i class="ri-star-line place__rating-icon"></i>
+                        <span class="place__rating-number">5,0</span>
+                    </span>
+
+                    <div class="place__data">
+                        <h3 class="place__title">Bora Bora</h3>
+                        <span class="place__subtitle">Polinesia</span>
+                        <span class="place__price">$1599</span>
+                    </div>
+                </div>
+
+                <button class="button button--flex place__button">
+                    <i class="ri-arrow-right-line"></i>
+                </button>
+            </div>
+
+            <!--==================== PLACES CARD 3 ====================-->
+            <div class="place__card">
+                <img src="assets/img/place3.jpg" alt="" class="place__img">
+
+                <div class="place__content">
+                    <span class="place__rating">
+                        <i class="ri-star-line place__rating-icon"></i>
+                        <span class="place__rating-number">4,9</span>
+                    </span>
+
+                    <div class="place__data">
+                        <h3 class="place__title">Hawaii</h3>
+                        <span class="place__subtitle">EE.UU</span>
+                        <span class="place__price">$3499</span>
+                    </div>
+                </div>
+
+                <button class="button button--flex place__button">
+                    <i class="ri-arrow-right-line"></i>
+                </button>
+            </div>
+
+            <!--==================== PLACES CARD 4 ====================-->
+            <div class="place__card">
+                <img src="assets/img/place4.jpg" alt="" class="place__img">
+
+                <div class="place__content">
+                    <span class="place__rating">
+                        <i class="ri-star-line place__rating-icon"></i>
+                        <span class="place__rating-number">4,8</span>
+                    </span>
+
+                    <div class="place__data">
+                        <h3 class="place__title">Whitehaven</h3>
+                        <span class="place__subtitle">Australia</span>
+                        <span class="place__price">$2499</span>
+                    </div>
+                </div>
+
+                <button class="button button--flex place__button">
+                    <i class="ri-arrow-right-line"></i>
+                </button>
+            </div>
+
+            <!--==================== PLACES CARD 5 ====================-->
+            <div class="place__card">
+                <img src="assets/img/place5.jpg" alt="" class="place__img">
+
+                <div class="place__content">
+                    <span class="place__rating">
+                        <i class="ri-star-line place__rating-icon"></i>
+                        <span class="place__rating-number">4,8</span>
+                    </span>
+
+                    <div class="place__data">
+                        <h3 class="place__title">Hvar</h3>
+                        <span class="place__subtitle">Croacia</span>
+                        <span class="place__price">$1999</span>
+                    </div>
+                </div>
+
+                <button class="button button--flex place__button">
+                    <i class="ri-arrow-right-line"></i>
+                </button>
+            </div>
+        </div>
+    </section>
+
+    <!--==================== SUBSCRIBE ====================-->
+    <section class="subscribe section">
+        <div class="subscribe__bg">
+            <div class="subscribe__container container">
+                <h2 class="section__title subscribe__title">Subscribe Our <br> Newsletter</h2>
+                <p class="subscribe__description">Subscribe to our newsletter and get a
+                    special 30% discount.
+                </p>
+
+                <form action="" class="subscribe__form">
+                    <input type="text" placeholder="Enter email" class="subscribe__input">
+
+                    <button class="button">
+                        Subscribe
+                    </button>
+                </form>
+            </div>
+        </div>
+    </section>
+
+    <!--==================== SPONSORS ====================-->
+    <section class="sponsor section">
+        <div class="sponsor__container container grid">
+            <div class="sponsor__content">
+                <img src="assets/img/sponsors1.png" alt="" class="sponsor__img">
+            </div>
+            <div class="sponsor__content">
+                <img src="assets/img/sponsors2.png" alt="" class="sponsor__img">
+            </div>
+            <div class="sponsor__content">
+                <img src="assets/img/sponsors3.png" alt="" class="sponsor__img">
+            </div>
+            <div class="sponsor__content">
+                <img src="assets/img/sponsors4.png" alt="" class="sponsor__img">
+            </div>
+            <div class="sponsor__content">
+                <img src="assets/img/sponsors5.png" alt="" class="sponsor__img">
+            </div>
+        </div>
+    </section>
+</main>
+
+<?php include('footer.php'); ?>
